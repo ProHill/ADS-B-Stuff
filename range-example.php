073905de3f11d16b0e3e763d14660f13c00ac492
Andy Hill <andy@hillhome.org>
2015-07-14 08:29:59 -0500

    Refactor range code and sort json from VRS

--- range-example.php
+++ range-example.php
@@ -6,11 +6,6 @@ echo '<html>
 $data = file_get_contents("/srv/www/htdocs/flights/range.json");
 $data = json_decode($data, true); 
 
-$polarplots1 = $data["PolarPlotSlices"][1]["PolarPlots"];
-$polarplots2 = $data["PolarPlotSlices"][2]["PolarPlots"];
-$polarplots3 = $data["PolarPlotSlices"][3]["PolarPlots"];
-$polarplots4 = $data["PolarPlotSlices"][4]["PolarPlots"];
-
 echo '
 <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css"></link>
 <link rel="stylesheet" href="range.css">
@@ -27,44 +22,23 @@ echo '
 	}
   
 	var map = new google.maps.Map(document.getElementById("range-map"), mapOptions);
-  	var rangePlot1 = [
   	';
-foreach($polarplots1 as $polarplot) {
-	if ($polarplot["Latitude"] && $polarplot["Longitude"]) {
-		echo 'new google.maps.LatLng(' . $polarplot["Latitude"] . ', ' . $polarplot["Longitude"] . '), ';
-		}
-	}	
-echo ']; ';
-echo '	
-  	var rangePlot2 = [
+for ($i = 1; $i <= 4; $i++) { 
+    $polarplots[$i] = $data["PolarPlotSlices"][$i]["PolarPlots"];
+	ksort($polarplots[$i]);
+	echo 'var rangePlot' . $i . ' = [
   	';
-foreach($polarplots2 as $polarplot) {
-	if ($polarplot["Latitude"] && $polarplot["Longitude"]) {
-		echo 'new google.maps.LatLng(' . $polarplot["Latitude"] . ', ' . $polarplot["Longitude"] . '), ';
+
+	foreach($polarplots[$i] as $polarplot) {
+		if ($polarplot["Latitude"] && $polarplot["Longitude"]) {
+			echo 'new google.maps.LatLng(' . $polarplot["Latitude"] . ', ' . $polarplot["Longitude"] . '), ';
+			}
 		}
-	}	
-echo ']; ';
-echo '	
-  	var rangePlot3 = [
-  	';
-foreach($polarplots3 as $polarplot) {
-	if ($polarplot["Latitude"] && $polarplot["Longitude"]) {
-		echo 'new google.maps.LatLng(' . $polarplot["Latitude"] . ', ' . $polarplot["Longitude"] . '), ';
-		}
-	}	
-echo ']; ';
-echo '	
-  	var rangePlot4 = [
-  	';
-foreach($polarplots4 as $polarplot) {
-	if ($polarplot["Latitude"] && $polarplot["Longitude"]) {
-		echo 'new google.maps.LatLng(' . $polarplot["Latitude"] . ', ' . $polarplot["Longitude"] . '), ';
-		}
-	}	
-  	  	
-  	
-echo '
-  ];
+	echo ']; 
+	';
+}
+
+echo '  
 rangePolygon1 = new google.maps.Polygon({
     paths: rangePlot1,
     strokeColor: "#FFFFFF",
@@ -103,7 +77,7 @@ rangePolygon4 = new google.maps.Polygon({
     fillColor: "#FF0000",
     fillOpacity: 0.2
   });
-	rangePolygon4.setMap(map);
+	rangePolygon4.setMap(map);		
 
 var rangeRings = [92600, 185200, 277800, 370400, 463000, 555600];
 var index;
@@ -120,13 +94,13 @@ for	(index = 0; index < rangeRings.length; index++) {
 	 rangeCircle = new google.maps.Circle(rangeOptions);
 }
 
-
 map.controls[google.maps.ControlPosition.LEFT_BOTTOM].push(
   document.getElementById("legend"));
+  }
 
-}
 
-google.maps.event.addDomListener(window, "load", initialize);
+  
+  google.maps.event.addDomListener(window, "load", initialize);
 </script>
 </head>
 <body>
